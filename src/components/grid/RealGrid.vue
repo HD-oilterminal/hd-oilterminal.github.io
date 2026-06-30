6b63182be91a2c96e5da4a747fa4e06ad2e535fb
green <greenn.lab@gmail.com>
2026-06-30 16:18:53 +0900

    feat: 그리드 컨텍스트 메뉴 (틀고정, 검색)

--- src/components/grid/RealGrid.vue
+++ src/components/grid/RealGrid.vue
@@ -2,9 +2,11 @@
 import type { ClickData, RowObject } from 'realgrid'
 import { GridBase, GridView, LocalDataProvider } from 'realgrid'
 import { computed, onBeforeUnmount, onMounted, ref, watch } from 'vue'
+import { useI18n } from 'vue-i18n'
 
+import Button from '../../components/commons/Button.vue'
 import { useRealGrid } from '../../composables/useRealGrid'
-import type { GridProps, Rows } from '../../types/core'
+import type { GridProps } from '../../types/core'
 import Pagination from '../commons/Pagination.vue'
 import { useGrid } from './RealGridOptions'
 
@@ -13,8 +15,7 @@ const props = withDefaults(defineProps<GridProps>(), {
   rows: () => [],
   height: '100%',
   editable: false,
-  checkable: false,
-  excel: () => ({ bridge: () => {}, get: () => {} })
+  checkable: false
 })
 
 const emit = defineEmits<{
@@ -24,40 +25,68 @@ const emit = defineEmits<{
   paging: [page: number]
 }>()
 
-const container = ref<HTMLDivElement>()
-const pageable = computed(() => {
-  return !Array.isArray(props.rows) && // 배열 형태
-    typeof props.rows.page === 'number' // page 속성 포함 및 숫자 타입
-    ? props.rows
-    : undefined
-})
+const { resolveColumns } = useRealGrid()
+const { gridish } = useGrid()
+const { t } = useI18n()
 
 let core: GridView
 let data: LocalDataProvider
 
-const { resolveColumns } = useRealGrid()
-const { gridish } = useGrid()
+const container = ref<HTMLDivElement>()
+const pageable = computed(() => {
+  return !Array.isArray(props.rows) && // 배열 형태 &
+    typeof props.rows.page === 'number' // page 속성 포함 및 숫자 타입 이면, 페이지처리 Ok
+    ? props.rows
+    : undefined
+})
+
+const searchText = ref('')
+const searchInput = ref<HTMLInputElement>()
+const searchPanel = ref(false)
+const searchPosition = ref({ x: 0, y: 0 })
+
+const doSearch = (reverse = false) => {
+  if (!searchText.value || !core || !data) return
+
+  const fields = data.getFieldNames()
+  const result = core.searchCell({
+    fields,
+    value: searchText.value,
+    startIndex: searchPosition.value.y,
+    startFieldIndex: searchPosition.value.x + (reverse ? -1 : 1),
+    wrap: true,
+    caseSensitive: false,
+    partialMatch: true,
+    reverse
+  })
+
+  if (result != null) {
+    core.setCurrent(result)
+    searchPosition.value = { x: result.fieldIndex, y: result.itemIndex }
+  }
+}
+
+const excel = (filename?: string) => {
+  console.log('excel', filename || props.title)
+
+  core.exportGrid({
+    type: 'excel',
+    target: 'local',
+    fileName: filename ?? props.title,
+    done: () => console.info('Excel Export Done')
+  })
+}
+
+watch(
+  () => props.rows,
+  rows => data?.setRows(Array.isArray(rows) ? rows : rows.list)
+)
 
 onMounted(() => {
   ;({ grid: core, provider: data } = gridish(props.title, container.value, {
     ...props,
     columns: resolveColumns(props.columns)
   }))
-
-  props.excel.bridge(function (rows?: Rows, filename?: string) {
-    let raws: Rows
-    if (rows) {
-      raws = data.getRows()
-      data.setRows(rows)
-    }
-
-    core.exportGrid({
-      type: 'excel',
-      target: 'local',
-      fileName: filename ?? props.title,
-      done: () => raws && data.setRows(raws)
-    })
-  })
 
   core.onCurrentChanged = (_g, newIndex) => {
     emit('currentChanged', newIndex.itemIndex ?? 0, core?.getCurrent().fieldName ?? '')
@@ -67,12 +96,27 @@ onMounted(() => {
     emit('cellClicked', value, grid)
     if (value.dataRow != undefined) emit('rowClicked', data.getJsonRow(value.dataRow), value, grid)
   }
-})
 
-watch(
-  () => props.rows,
-  rows => data?.setRows(Array.isArray(rows) ? rows : rows.list)
-)
+  core.setContextMenu([
+    { label: t('엑셀 다운로드'), name: 'excel' },
+    { label: t('검색'), name: 'search' },
+    { label: t('틀고정'), name: 'freeze' }
+  ])
+
+  core.onContextMenuItemClicked = (_, menu, cell) => {
+    if (menu.name === 'excel') excel()
+    else if (menu.name === 'search') {
+      searchText.value = ''
+      searchPanel.value = true
+      searchPosition.value = { x: cell.field ?? 0, y: cell.itemIndex ?? 0 }
+
+      setTimeout(() => searchInput.value?.select(), 50)
+    } else if (menu.name === 'freeze') {
+      const index = core.getColumnNames(true, false).indexOf(cell.column ?? '')
+      if (index >= 0) core.setFixedOptions({ colCount: index })
+    }
+  }
+})
 
 onBeforeUnmount(() => {
   data?.clearRows()
@@ -92,6 +136,18 @@ defineExpose({
 <template>
   <div class="realgrid-wrapper" :style="{ height }">
     <div ref="container" class="realgrid-container" />
+    <Transition name="fade">
+      <div v-if="searchPanel" class="realgrid-search-layer">
+        <input
+          ref="searchInput"
+          v-model="searchText"
+          :placeholder="`${$t('검색어')} Enter`"
+          @keydown.enter="doSearch($event.shiftKey)"
+          @keydown.esc="searchPanel = false"
+        />
+        <Button variant="ghost" type="button" @click="searchPanel = false">{{ $t('닫기') }}</Button>
+      </div>
+    </Transition>
     <div v-if="pageable" class="realgrid-pagination">
       <Pagination
         :model-value="pageable.page"
--- src/components/grid/RealTree.vue
+++ src/components/grid/RealTree.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import type { CellIndex, ClickData, GridBase, LocalTreeDataProvider, RowObject, TreeView } from 'realgrid'
 import { computed, onBeforeUnmount, onMounted, ref, watch } from 'vue'
+import { useI18n } from 'vue-i18n'
 
+import Button from '../../components/commons/Button.vue'
 import { useRealGrid } from '../../composables/useRealGrid'
-import type { Rows, TreeProps } from '../../types/core'
+import type { TreeProps } from '../../types/core'
 import Pagination from '../commons/Pagination.vue'
 import { useGrid } from './RealGridOptions'
 
@@ -22,78 +24,133 @@ const emit = defineEmits<{
   paging: [page: number]
 }>()
 
-const container = ref<HTMLDivElement>()
-let grid: TreeView
+const { resolveColumns } = useRealGrid()
+const { treeish } = useGrid()
+const { t } = useI18n()
+
+let core: TreeView
 let data: LocalTreeDataProvider
 
+const container = ref<HTMLDivElement>()
 const pageable = computed(() => {
-  return !Array.isArray(props.rows) && // 배열 형태
-    typeof props.rows.page === 'number' // page 속성 포함 및 숫자 타입
+  return !Array.isArray(props.rows) && // 배열 형태 &
+    typeof props.rows.page === 'number' // page 속성 포함 및 숫자 타입 이면, 페이지처리 Ok
     ? props.rows
     : undefined
 })
-const list = computed<Rows>(() => (pageable.value ? pageable.value.list : (props.rows as Rows)))
 
-const { resolveColumns } = useRealGrid()
-const { treeish } = useGrid()
+const searchText = ref('')
+const searchInput = ref<HTMLInputElement>()
+const searchPanel = ref(false)
+const searchPosition = ref({ x: 0, y: 0 })
+
+const doSearch = (reverse = false) => {
+  if (!searchText.value || !core || !data) return
+
+  const fields = data.getFieldNames()
+  const result = core.searchCell({
+    fields,
+    value: searchText.value,
+    startIndex: searchPosition.value.y,
+    startFieldIndex: searchPosition.value.x + (reverse ? -1 : 1),
+    wrap: true,
+    caseSensitive: false,
+    partialMatch: true,
+    reverse
+  })
+
+  if (result != null) {
+    core.setCurrent(result)
+    searchPosition.value = { x: result.fieldIndex, y: result.itemIndex }
+  }
+}
+
+const excel = (filename?: string) => {
+  console.log('excel', filename || props.title)
+
+  core.exportGrid({
+    type: 'excel',
+    target: 'local',
+    fileName: filename ?? props.title,
+    done: () => console.info('Excel Export Done')
+  })
+}
+
+watch(
+  () => props.rows,
+  rows => {
+    data?.setNestedRows({ rows: Array.isArray(rows) ? rows : rows.list }, 'rows', props.treeColumnKey ?? '')
+    if (props.expanded) core?.expandAll()
+  }
+)
 
 onMounted(() => {
-  ;({ grid, provider: data } = treeish('Sample Title', container.value, {
+  ;({ grid: core, provider: data } = treeish('Sample Title', container.value, {
     ...props,
     columns: resolveColumns(props.columns)
   }))
 
-  grid.onCurrentChanged = (grid: GridBase, index: CellIndex) => {
+  core.onCurrentChanged = (grid: GridBase, index: CellIndex) => {
     emit('currentChanged', index.itemIndex ?? 0, grid?.getCurrent().fieldName ?? '')
   }
 
-  grid.onCellClicked = (grid, value) => {
+  core.onCellClicked = (grid, value) => {
     emit('cellClicked', value, grid)
 
     if (value.dataRow != undefined) emit('rowClicked', data.getJsonRow(value.dataRow), value, grid)
   }
-})
 
-watch(
-  () => props.rows,
-  () => {
-    data?.setNestedRows({ rows: list.value }, 'rows', props.treeColumnKey ?? '')
-    if (props.expanded) grid?.expandAll()
+  core.setContextMenu([
+    { label: t('엑셀 다운로드'), name: 'excel' },
+    { label: t('검색'), name: 'search' },
+    { label: t('틀고정'), name: 'freeze' }
+  ])
+
+  core.onContextMenuItemClicked = (_, menu, cell) => {
+    if (menu.name === 'excel') excel()
+    else if (menu.name === 'search') {
+      searchText.value = ''
+      searchPanel.value = true
+      searchPosition.value = { x: cell.field ?? 0, y: cell.itemIndex ?? 0 }
+
+      setTimeout(() => searchInput.value?.select(), 50)
+    } else if (menu.name === 'freeze') {
+      const index = core.getColumnNames(true, false).indexOf(cell.column ?? '')
+      if (index >= 0) core.setFixedOptions({ colCount: index })
+    }
   }
-)
+})
 
 onBeforeUnmount(() => {
   data?.clearRows()
-  grid?.destroy()
+  core?.destroy()
 })
 
-const excel = (rows?: Rows, filename?: string) => {
-  if (rows) data.setNestedRows({ rows }, 'rows', props.treeColumnKey ?? '')
-
-  grid.exportGrid({
-    type: 'excel',
-    target: 'local',
-    fileName: filename ?? props.title,
-    done: () => {
-      if (rows) data.setNestedRows({ rows: list.value }, 'rows', props.treeColumnKey ?? '')
-    }
-  })
-}
-
 defineExpose({
-  get core() {
-    return grid
+  get grid() {
+    return core
   },
   get data() {
     return data
-  },
-  excel
+  }
 })
 </script>
 
 <template>
   <div class="realgrid-wrapper" :style="{ width: '100%', height }">
     <div ref="container" class="realgrid-container" />
+    <Transition name="fade">
+      <div v-if="searchPanel" class="realgrid-search-layer">
+        <input
+          ref="searchInput"
+          v-model="searchText"
+          :placeholder="`${$t('검색어')} Enter`"
+          @keydown.enter="doSearch"
+          @keydown.esc="searchPanel = false"
+        />
+        <Button variant="ghost" type="button" @click="searchPanel = false">{{ $t('닫기') }}</Button>
+      </div>
+    </Transition>
     <div v-if="pageable" class="realgrid-pagination">
       <Pagination
         :model-value="pageable.page"
